0a8f981af60c2c7d2134a82fb470c48b44797447
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-04 19:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing(array,  changeValue, skipValue){
 function findBy(array, findFn){
   for(let i = 0; i < array.length; i++){
     let result = findFn(array[i]);
-    if(result){
+    if(true){
       return array[i];
       
     }
